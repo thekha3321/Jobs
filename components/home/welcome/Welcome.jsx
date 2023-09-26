d0a88a7ab2c7ee1447d1162e29682a5782a4ef73
thekha3321 <thekha234@gmail.com>
2023-09-26 12:50:55 +0700

    home UI

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import styles from "./welcome.style";
+import { useRouter } from "expo-router";
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+};
+
+export default Welcome;
